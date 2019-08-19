4e3b6d73786157fb8ecf71440cac38ccf069de5d
akhilcm <akhilcm777@gmail.com>
2019-08-19 22:03:13 +0530

    Update

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 const Express = require('express');
+
+var bodyParser= require('body-parser');
+
 var app=new Express();
 app.set('view engine','ejs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
 app.get('/',(req,res)=>{
     res.render('home');
 });
 
 app.get('/login',(req,res)=>{
     res.render('login');
+});
+
+// app.post('/read',(req,res)=>{
+//     var body = req.body;
+//     res.render('loginread',{log:body});
+// });
+
+app.post('/read',(req,res)=>{
+    var re = req.body;
+    res.render('registrationread',{regi:re});
 });
 
 app.get('/regi',(req,res)=>{
