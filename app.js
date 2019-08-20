ec82a2714929c2621e2e47fdbaf0a0818b5c0026
akhilcm <akhilcm777@gmail.com>
2019-08-21 00:02:44 +0530

    database insert display

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const Express = require('express');
+const Mongoose = require('mongoose');
+var request = require('request');
+const Registration=Mongoose.model("registrationdetails",{
+    name: String ,
+    address: String,
+    gen:String,
+    dis: String,
+    dob: String,
+    email:String,
+    uname: String,
+    psd:String,
+    cpsd: String});
+
+
+Mongoose.connect("mongodb://localhost:27017/regidb");
 
 var bodyParser= require('body-parser');
-
 var app=new Express();
 app.set('view engine','ejs');
 
@@ -16,19 +30,47 @@ app.get('/login',(req,res)=>{
     res.render('login');
 });
 
-// app.post('/read',(req,res)=>{
-//     var body = req.body;
-//     res.render('loginread',{log:body});
-// });
 
-app.post('/read',(req,res)=>{
-    var re = req.body;
-    res.render('registrationread',{regi:re});
-});
 
 app.get('/regi',(req,res)=>{
     res.render('registration');
 });
+
+app.post('/read',(req,res)=>{
+    console.log(req.body);
+    var regis= Registration(req.body);
+    var result = regis.save( (error)=>{
+        if(error){
+            throw error;
+            res.send(error);
+        }
+        else{
+            res.send('user created');
+        }
+    });
+});
+
+app.get('/getdatas',(req,res)=>{
+    result = Registration.find( (error,data)=>{
+         if(error){
+             throw error;
+         }
+         else{
+             res.send(data);
+         }
+     });
+});
+
+const getdataApi="http://localhost:3004/getdatas";
+
+app.get('/views',(req,res)=>{
+    request(getdataApi,(error,response,body)=>{
+        var data=JSON.parse(body);
+        console.log(data);
+        res.render('regiview',{'data':data});
+    });
+});
+
 app.listen(3004,()=>{
-    console.log("server running on port 3004");
+    console.log("server running on port: http://localhost:3004");
 });
